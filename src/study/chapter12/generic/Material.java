5a9738da3d419a1740c69948f83c53dcbc8c1906
doosam <doosam@skoopmedia>
2020-09-23 22:45:23 +0900

    Initial commit

---
+++ src/study/chapter12/generic/Material.java
@@ -0,0 +1,5 @@
+package study.chapter12.generic;
+
+public abstract class Material {
+    public abstract void doPrinting();
+}
---
+++ src/study/finaly/grade/GradeEvaluation.java
@@ -0,0 +1,5 @@
+package study.finaly.grade;
+
+public interface GradeEvaluation {
+    public String getGrade(int point);
+}
